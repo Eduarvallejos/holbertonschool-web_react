b234a320de63a0f5bfbe23f9a2a468242e565517
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-05-12 13:38:51 -0500

    webpack config

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+
+module.exports = {
+    mode: 'production',
+    entry: './js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name:'[name].[ext]',
+                            outputPath: 'images/',
+                            publicPath: 'images/',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
