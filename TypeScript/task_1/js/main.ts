09e14dd15707bc6ec18cd70456be808623f16bda
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-05-13 00:03:43 -0500

    Let's build a Teacher interface

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -6,15 +6,3 @@ interface Teacher{
     location: string;
     [key: string]: string | boolean | number | undefined;
 };
-
-// Crear un objeto Teacher
-const teacher3: Teacher = {
-    firstName: "John",
-    lastName: "Doe",
-    fullTimeEmployee: true,
-    yearsOfExperience: 5,
-    location: "New York",
-    contract: true // Se agrega una propiedad adicional llamada "contract"
-};
-
-console.log(teacher3);
