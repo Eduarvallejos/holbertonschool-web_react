bce01972800e52a25e913af7883f9e1032759b1d
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-05-19 02:09:35 -0500

    add a close button

--- react_intro/task_2/dashboard/src/Notifications.js
+++ react_intro/task_2/dashboard/src/Notifications.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React from 'react';
 import './Notifications.css';
+import { getLatestNotification } from './utils';
+
+const handleClick = () => {
+  console.log('Close button has been clicked');
+};
 
 const Notifications = () => {
+
   return (
     <div className="Notifications">
+      <button
+          aria-label="Close"
+          onClick={handleClick}
+        style={{
+          backgroundColor: 'transparent',
+          border: 'none',
+          cursor: 'pointer',
+          float: 'right',
+        }}
+      >
+        x
+      </button>
       <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+      </ul>
     </div>
   );
-}
+};
 
 export default Notifications;
