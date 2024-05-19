c1c20c0a8669f652b5797c9b3c09bf99cb4ff578
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-05-19 00:43:50 -0500

    add email and password

---
+++ react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,32 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+import {getFullYear, getFooterCopy} from './utils'
+
+function App() {
+  const isIndex = true;
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email"/>
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password"/>
+
+        <button>OK</button>
+      </div>
+      <div className="App-footer">
+        <p>{getFooterCopy(isIndex)}</p>
+        <p>© {getFullYear()}</p>
+      </div>
+    </div>
+    )
+}
+
+export default App;
