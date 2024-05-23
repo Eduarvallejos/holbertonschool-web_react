40c17f98dda3f0a0889aa851fe8d7d44879be44e
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-05-22 22:24:44 -0500

    Update setupTests.js

--- react_intro/task_3/dashboard/src/setupTests.js
+++ react_intro/task_3/dashboard/src/setupTests.js
@@ -1,4 +1,4 @@
-// This script configure the adapter for Enzyme..
+// This script configure the adapter for Enzyme.
 import '@testing-library/jest-dom';
 import { configure } from 'enzyme';
 import Adapter from '@cfaester/enzyme-adapter-react-18';
