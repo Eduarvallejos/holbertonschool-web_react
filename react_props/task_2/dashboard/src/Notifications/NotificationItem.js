37f69cabd73cd12fb1cc86258e9388e76899af70
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-05-27 00:41:23 -0500

    changes the way a <li> is generated

---
+++ react_props/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const NotificationItem = ({ type, html, value }) => {
+  if (html) {
+    return (
+      <li data-notification-type={type} dangerouslySetInnerHTML={html}></li>
+    );
+  }
+  return (
+    <li data-notification-type={type}>{value}</li>
+  );
+};
+
+NotificationItem.propTypes = {
+  type: PropTypes.string.isRequired,
+  html: PropTypes.shape({
+    __html: PropTypes.string,
+  }),
+  value: PropTypes.string,
+};
+
+NotificationItem.defaultProps = {
+  type: 'default',
+  html: null,
+  value: '',
+};
+
+export default NotificationItem;
---
+++ react_props/task_2/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotificationItem from './NotificationItem';
+
+describe('NotificationItem Component', () => {
+  it('renders without crashing', () => {
+    shallow(<NotificationItem type="default" value="test" />);
+  });
+
+  it('renders correct html with type and value props', () => {
+    const type = 'default';
+    const value = 'test';
+    const wrapper = shallow(<NotificationItem type={type} value={value} />);
+    expect(wrapper.find('li').prop('data-notification-type')).toEqual(type);
+    expect(wrapper.text()).toContain(value);
+  });
+
+  it('renders correct HTML with dummy html prop', () => {
+    const html = { __html: '<u>test</u>' };
+    const wrapper = shallow(<NotificationItem html={html} />);
+    expect(wrapper.find('li').prop('dangerouslySetInnerHTML')).toEqual(html);
+  });
+});
