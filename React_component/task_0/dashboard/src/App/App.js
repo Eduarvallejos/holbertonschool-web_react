f4dfcdafadadb7a7af65329ce7accc4cd5b8d5a6
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-06-10 00:07:36 -0500

    React class

---
+++ React_component/task_0/dashboard/src/App/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import Notifications from '../Notifications/Notifications';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import './App.css'
+
+const isIndex = true;
+class App extends Component{
+  render () {
+    return (
+      <React.Fragment>
+        <Notifications />
+        <div className="App">
+          <Header />
+          <Login />
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
