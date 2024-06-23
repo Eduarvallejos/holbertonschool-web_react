e630b084730de44eb55b750b5ce137335e4ee9bd
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-06-23 13:24:38 -0500

    Commence with class components

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
 import Notifications from '../Notifications/Notifications';
@@ -7,17 +7,28 @@ import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import CourseList from '../CourseList/CourseList';
 
-class App extends Component {
-  render() {
-    const { isLoggedIn = false } = this.props;
+const listCourses = [
+  {id: 1, name: 'ES6', credit: 60},
+  {id: 2, name: 'Webpack', credit: 20},
+  {id: 3, name: 'React', credit: 40},
+];
 
+const listNotifications = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
+];
+
+class App extends Component{
+  render() {
+    const {isLoggedIn = false} = this.props
     return (
       <div className="App">
         <div className="headerWithNotifications">
-          <Notifications />
+          <Notifications listNotifications={listNotifications}/>
           <Header />
         </div>
-        {isLoggedIn ? <CourseList /> : <Login />}
+        {isLoggedIn ? <CourseList listCourses={listCourses}/> : <Login />}
         <Footer />
       </div>
     );
