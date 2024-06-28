b08d20f91039e0a95a8bb632692b721580323029
Eduarvallejos <manuelvallejos2018@gmail.com>
2024-06-27 23:15:13 -0500

    1. install Aphrodite

--- React_inline_styling/task_1/dashboard/src/Login/Login.js
+++ React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -1,53 +1,30 @@
 import React from "react";
 import { StyleSheet, css } from 'aphrodite';
+//import './Login.css';
 
 const styles = StyleSheet.create({
     AppBody: {
         flex: '1',
         padding: '10px',
         paddingLeft: '20px',
-        fontFamily: '"Segoe UI", sans-serif',
+        fontFamily: '"segoe IU", sans-serif',
         margin: '10px',
     },
-    formGroup: {
-        marginBottom: '15px',
-    },
-    label: {
-        display: 'block',
-        marginBottom: '5px',
-    },
-    input: {
-        display: 'block',
-        width: '10%',
-        padding: '8px',
-        marginBottom: '10px',
-        border: '1px solid #ccc',
-        borderRadius: '4px',
-    },
-    button: {
-        padding: '10px 15px',
-        backgroundColor: '#007bff',
-        color: '#fff',
-        border: 'none',
-        borderRadius: '4px',
-        cursor: 'pointer',
-    }
 });
 
 const Login = () => {
     return (
-        <div className={css(styles.AppBody)}>
-            <p>Login to access the full dashboard</p>
-            <div className={css(styles.formGroup)}>
-                <label htmlFor="email" className={css(styles.label)}>Email:</label>
-                <input type="email" id="email" className={css(styles.input)} />
+        <React.Fragment>
+            <div className={css(styles.AppBody)}>
+                <p>Login to access the full dashboard</p>
+                <label htmlFor="email"> Email: </label>
+                <input type="email" id="email"/>
+                <label htmlFor="password"> Password: </label>
+                <input type="password" id="password"/>
+
+                <button>OK </button>
             </div>
-            <div className={css(styles.formGroup)}>
-                <label htmlFor="password" className={css(styles.label)}>Password:</label>
-                <input type="password" id="password" className={css(styles.input)} />
-            </div>
-            <button className={css(styles.button)}>OK</button>
-        </div>
+        </React.Fragment>
     );
 };
 
